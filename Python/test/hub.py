0ccb56c3d7ad83cc217300011cd27ef74ac09114
wangyafei <w7297911@163.com>
2018-04-02 01:10:45 +0800

    更新hub文件

--- Python/test/hub.py
+++ Python/test/hub.py
@@ -1,13 +1,17 @@
+#coding = utf-8
+#测试
+for i in range(0,8):
 
-i=0
-while (i<8):
-    j=0
-    while (j<8):
+    for j in range(0,8):
         OA = int(abs(1 - i) + abs(7 - j))
         OB = int(abs(8 - i) + abs(6 - j))
         OC = int(abs(6 - i) + abs(3 - j))
         OD = int(abs(2 - i) + abs(0 - j))
-        j += 1
-    i += 1
     sum = int(OA +OB +OC +OD)
     print ("sum = %d"%sum)
+    print("*"*40)
+print("$"*40)
+for feiers in range(1,10):
+    print(type(feiers))
+    print("上面的计算过程好像是错误的 %2d"%feiers)
+
