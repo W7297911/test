6141ba40835f5bb94e4641d2fa39097f8d75ecaa
W7297911 <W7297911@163.COM>
2018-05-24 12:49:30 +0800

    gengxin

--- Python/firstPython/text1.py
+++ Python/firstPython/text1.py
@@ -1,4 +1,4 @@
-
+#coding:utf-8
 import requests
 '''keyword = 'python'
 try:
@@ -28,9 +28,25 @@ try:
 except:
 	print('爬去失败')
 '''
-r = requests.get('http://python123.io/ws/demo.html')
+'''r = requests.get('http://python123.io/ws/demo.html')
 print(r.text)
 demo = r.text
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(demo,'html.parser')
-print(soup.prettify())
+print(soup.prettify())'''
+
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['simhei']
+plt.rcParams['axes.unicode_minus']=False
+y1=[10,13,25,40,30,50,40,38,40,35,29,34,38,21,34,35,42,12,34,45]
+x1=range(0,20)
+x2=range(0,20)
+y2=[5,8,10,30,20,40,30,31,38,30,28,23,34,12,32,22,32,12,23,34]
+plt.plot(x1,y1,label='Frist line',linewidth=3,color='r',marker='o',
+markerfacecolor='blue',markersize=5)
+plt.plot(x2,y2,label='second line')
+plt.xlabel('日期')
+plt.ylabel('销量')
+plt.title('中国香港公司\n销售数据')
+plt.legend()
+plt.show()
