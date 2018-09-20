2764f8112b3b3283b3f105dd77e212896c88493e
wangyafei <w7297911@163.com>
2018-09-21 00:02:12 +0800

    更新内容
    Signed-off-by: wangyafei <w7297911@163.com>

--- Python/firstPython/text.py
+++ Python/firstPython/text.py
@@ -2,7 +2,7 @@
 def fahrenheit_converter(c):
     fahrenheit = c * 9/5 + 32
     return str(fahrenheit) + "℉"
-#请输入需要转换的摄氏度
+# 请输入需要转换的摄氏度
 rename = input("请输入转换的摄氏度：")
 C2F = fahrenheit_converter(rename)
 print(C2F)
