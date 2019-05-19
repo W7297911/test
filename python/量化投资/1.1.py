21f213a2aa8db91453bb23e06f21104fd5707b5f
wangyafei <w7297911@163.com>
2019-05-19 16:15:14 +0800

    量化投资提交

---
+++ python/量化投资/1.1.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+# Version:python3.6.0
+# Tools:Pycharm 2017.3.2
+# Date:2019-05-18 23:29
+# Author:Wangyafei
+
+
+import pandas as p
+import numpy as n
---
+++ python/量化投资/err.py
@@ -0,0 +1,4 @@
+# conda异常处理
+# RemoveError: 'setuptools' is a dependency of conda and cannot be removed from conda's operating environment.
+# 执行以下语句
+# conda install -c anaconda setuptools
