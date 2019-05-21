0cd94c20cb5e9d329de5a0054dd027c73e79419a
wangyafei <w7297911@163.com>
2019-05-21 15:27:36 +0800

    更新文件

--- python/量化投资/1.1.py
+++ python/量化投资/1.1.py
@@ -4,6 +4,12 @@
 # Date:2019-05-18 23:29
 # Author:Wangyafei
 
-
-import pandas as p
+import pandas as pd
 import numpy as n
+
+# 读取CSV文件
+data = pd.read_csv('E:\SRCB\Money.csv', encoding='gbk')
+# 读取CSV文件的某一列
+data = data['m']
+# data['today']=pd.to_datetime('2019-05-21')
+print(data)
